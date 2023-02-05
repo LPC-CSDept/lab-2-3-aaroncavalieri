8886446c1bf501d2ac7429124c7107dc8782f04f
Your Name/Applications/anaconda3/bin/python <you@example.com>
2023-02-04 18:57:47 -0800

    test to push 2

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ def main():
     sum = int(val1) + int(val2) + int(val3) #added int incase input field is used
     avg = sum / 3 
 
+    
 
-    
-    
     print ("Values: {0:2d} {1:3d} {2:4d}" .format(val1, val2, val3))
     print ("Total:  \t {0:.0f} ".format(sum))
     print ("Average: \t {0:.2f} ".format(avg)) #\t means skip over next horizontal tab.{0:}.2f} 0 means 1st position and .2f means two decimal places.
