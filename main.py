e47d16715fc2b341805e62c08ccb14e353dc6742
Your Name/Applications/anaconda3/bin/python <you@example.com>
2023-02-04 18:45:51 -0800

    V01

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 def main():
     ##################################################
-    # Comlete your code here
+    
+    val1 = 100
+    val2 = 90
+    val3 = 110
+    
+    """
+    val1 = input(("Value 1: "))
+    val2 = input(("Value 2: "))
+    val3 = input(("Value 3: "))
+    """
+    
+    sum = int(val1) + int(val2) + int(val3) #added int incase input field is used
+    avg = sum / 3 
+
+    
+    print ("Values: {0:2d} {1:3d} {2:4d}" .format(val1, val2, val3))
+    print ("Total:  \t {0:.0f} ".format(sum))
+    print ("Average: \t {0:.2f} ".format(avg)) #\t means skip over next horizontal tab.{0:}.2f} 0 means 1st position and .2f means two decimal places.
+
     ##################################################
 
     pass
